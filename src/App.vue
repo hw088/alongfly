5bcef89383547c74634d857ed8e163ea035f636d
hw088 <wetrik123@163.com>
2021-09-16 00:13:27 +0800

    feat: project test

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="app" class="wrapper">
-    <router-view/>
-    <main-tab-bar/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    name: 'app',
-    components: {
-      MainTabBar
-    }
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
   }
+}
 </script>
 
 <style>
-  @import "assets/css/base.css";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
